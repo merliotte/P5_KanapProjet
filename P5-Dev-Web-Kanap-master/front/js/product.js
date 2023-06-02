5b325f51113d81e8f8eadae55c97efb8fac988ab
merliotte <livainugent@hotmail.fr>
2023-06-02 13:02:24 +0200

    Mis à jours

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -1,24 +1,31 @@
-// Récupération des informations de la page source
-const urlParams = new URL(document.location).searchParams;
-// Ajout de l'ID 
-const urlId = urlParams.get("id");
-// Connexion avec l'API 
-const url = `http://localhost:3000/api/products/${urlId}`;
-const arrayId = [
+// ---------------------------------------------------------------
+// PERMET DE RECUPERER LES INFORMATIONS DES KANAPS
+// ---------------------------------------------------------------
+async function fetchKanapData() {
+	try {
+  	const kanapId = new URL(window.location).searchParams.get('id');
+  	const res = await fetch(`http://localhost:3000/api/products/${kanapId}`);
+      const canapData = await res.json();
+      return canapData;
+  } catch (error) {
+  	console.error(error.message);
+    window.alert (" La Connexion a échoué !");
+  }
+};
+// ---------------------------------------------------------------
+// PERMET D'AFFICHER LES INFORMATIONS DU KANAP
+// ---------------------------------------------------------------
+function fillHtmlWithCanapData(data) {
+	const arrayId = [
     {htmlElementId: "title",propsToLoadFromData:"name"}, 
     {htmlElementId: "description", propsToLoadFromData:"description"},
     {htmlElementId: "price", propsToLoadFromData:"price"},
     {htmlElementId: ".item__img > img", propsToLoadFromData:"imageUrl", useMethodByClass: true},
     {htmlElementId: ".item__img > img ", propsToLoadFromData:"altTxt"},
     {htmlElementId: "colors", propsToLoadFromData: "colors"}
-];
-//  Création de la function et Récupération des informations de l'API avec fetch
-function addElements() { 
-    // Ajout des informations depuis l'API
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        arrayId.forEach(addkanap => {
+	];
+  
+  arrayId.forEach(addkanap => {
             const img = document.querySelector(addkanap.htmlElementId);
            if(addkanap.useMethodByClass ) {
             img.setAttribute("src",data[addkanap.propsToLoadFromData]);
@@ -30,68 +37,67 @@ function addElements() {
         }
            document.getElementById(addkanap.htmlElementId).innerHTML = data[addkanap.propsToLoadFromData];
         });
-        // Lien avec le DOM et insertion de texte 
-        const select = document.querySelector("select");
-        select.innerHTML = `<option>Sélectionnez une couleur</option>`;
-        
+       
+     const select = document.querySelector("select");
+     select.innerHTML += `<option>Sélectionnez une couleur</option>`;
         // Réclamation des couleurs avec une boucle forEach
         data.colors.forEach(color => {
             select.innerHTML += `<option value="${color}">${color}</option>`;
-        }); 
-        // Stockage au click des informations dans le localStorage
-        const informationKanap = document.getElementById("addToCart");
-        const saveColor = document.getElementById("colors");
-        const quantityKanap = document.getElementById("quantity");
+
+        });  
+};
+
+function isCanapInLocalStorage(canapId) {
+  const cartItems = JSON.parse(window.localStorage.getItem("cartItems") || "[]");
+  return cartItems.findIndex((item) => item.id === canapId);
+  
+};
+
+// ---------------------------------------------------------------
+//  PERMET DE STOCKER LES ITEMS DANS LE LOCALSTORAGE
+// ---------------------------------------------------------------
+function addItemsIntoStorage(canapId) {
+  const saveColor = document.getElementById("colors");
+  const quantityKanap = document.getElementById("quantity");
+  const indexCanap = isCanapInLocalStorage(canapId);
+  const cartItems = JSON.parse(window.localStorage.getItem("cartItems") || "[]");
+  const newKanapData = {
+  	id: canapId,
+    quantity: Math.min(parseInt(quantityKanap.value), 100),
+    color: saveColor.value,
+  };
+  if (saveColor.value === "Sélectionnez une couleur" || quantity.value === "0")
+    return saveColor;
+
+  if (indexCanap >= 0) {
+  	cartItems[indexCanap] = newKanapData;
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    return;
+  }
+
+  cartItems.push(newKanapData);
+  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  if (newKanapData.quantity !== parseInt(quantityKanap.value)) {
+    console.log("La quantité a été limitée à 100.");
+  }
+};
+function controlQuantity() {
+	const quantity = document.querySelector('#quantity').value;
+    if (quantity < 0 ) document.querySelector('#quantity').value = 0;
+    if (quantity > 100) document.querySelector('#quantity').value = 100;
+};
+
+async function main() {
+    try {
         
-        // EVENT
-        informationKanap.addEventListener("click", () => {
-            const selectedColor = saveColor.value;
-            const selectedQuantity = quantityKanap.value;
-            console.log(selectedColor);
-            // Création de l'objet 
-            if (selectedColor !== "Sélectionnez une couleur" && selectedQuantity > 0 ) {
-                const cartItem = {
-                    id : urlId,
-                    color : selectedColor,
-                    quantity : selectedQuantity,
-                };
-                
-                // Récupérer les éléments de panier existants à partir du localstorage et les convertir en tableau   
-                let cartItems = JSON.parse(window.localStorage.getItem("cartItems") || "[]");
-                // Vérifier si un article de la même couleur existe déjà dans le panier
-                const existingItemIndex = cartItems.findIndex((item) => item.color === selectedColor && item.quantity === selectedQuantity );
-            if (existingItemIndex !== -1 )   {
-                // Mettre à jour la quantité de l'article existant
-                existingItemIndex.quantity += Number(selectedQuantity);
-            } else {
-                // Ajouter un nouvel article au tableau des articles du panier
-                cartItems = [cartItem];
-            }
-            // Stocker les articles du panier mis à jour dans le localstorage
-            localStorage.setItem("cartItems", JSON.stringify(cartItems));
-            
-            // Afficher quand le panier est rempli 
-            if (cartItems.length > 0) {
-                const panierRempli = document.getElementById("panier");
-                panierRempli.style.color = "red";
-            }
-        }
-    });
-    // Réclamation des items avec une boucle forEach
-
-      
-    console.log(data);
-});    
+        const canap = await fetchKanapData();
+        fillHtmlWithCanapData(canap);
+        document.querySelector('[name="itemQuantity"]').addEventListener('keyup', controlQuantity);
+        document.getElementById("addToCart").addEventListener('click', () => addItemsIntoStorage(canap._id)
+    );
+  } catch(error) {
+  	console.error(error.message);
+  }
 }
-// Appel à la fonction 
-addElements();
-console.log(url);
 
-
- 
-
-
-
-
-
-
+main();
